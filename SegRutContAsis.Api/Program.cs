a4694cdb9539851c1e553f72ea12a6ae8fe0356e
DiegoMolina65 <130077158+DiegoMolina65@users.noreply.github.com>
2025-11-18 17:25:40 -0400

    Refactor Program.cs for cleaner configuration

--- SegRutContAsis.Api/Program.cs
+++ SegRutContAsis.Api/Program.cs
@@ -1,129 +1,197 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 using Microsoft.EntityFrameworkCore;
+
 using Microsoft.IdentityModel.Tokens;
+
 using QuestPDF.Infrastructure;
+
 using SegRutContAsis.Business.Interfaces.AsignacionClienteVendedor;
+
 using SegRutContAsis.Business.Interfaces.AsignacionSupervisorVendedor;
+
 using SegRutContAsis.Business.Interfaces.Asistencia;
+
 using SegRutContAsis.Business.Interfaces.Authentication;
+
 using SegRutContAsis.Business.Interfaces.Cliente;
+
 using SegRutContAsis.Business.Interfaces.DireccionCliente;
+
 using SegRutContAsis.Business.Interfaces.Evidencia;
+
 using SegRutContAsis.Business.Interfaces.MarcarLlegadaVisita;
+
 using SegRutContAsis.Business.Interfaces.ReportesActividad;
+
 using SegRutContAsis.Business.Interfaces.Rol;
+
 using SegRutContAsis.Business.Interfaces.Ruta;
+
 using SegRutContAsis.Business.Interfaces.SeguimientoVendedor;
+
 using SegRutContAsis.Business.Interfaces.Visita;
+
 using SegRutContAsis.Business.Interfaces.Zona;
+
 using SegRutContAsis.Business.Services;
+
 using SegRutContAsis.Business.Services.Reporte;
+
 using SegRutContAsis.Business.Services.Rol;
-using System.Globalization;
+
 using System.Text;
-
+ 
 var builder = WebApplication.CreateBuilder(args);
-
-var culture = new CultureInfo("en-US");
-CultureInfo.DefaultThreadCurrentCulture = culture;
-CultureInfo.DefaultThreadCurrentUICulture = culture;
-
-
+ 
 // Add services to the container.
-
-// Configurar CORS con pol韙ica nombrada solo desarrollo
-/*
+ 
+// Configurar CORS con pol铆tica nombrada solo desarrollo
+ 
 builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend", policy =>
-    {
-        policy.WithOrigins(
-                "http://localhost:5173",    // tu front (http)
-                "https://localhost:5173"    // por si usas https en dev
-            )
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials(); 
-    });
-});
-*/
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
+{
+
+    options.AddPolicy("AllowFrontend", policy =>
+
     {
-        // Permite case-insensitive en nombres de propiedades JSON
-        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+
+        policy.WithOrigins(
+
+                "http://localhost:5173",    
+
+                "https://localhost:5173",
+
+                "https://diego.dualbiz.net"
+
+            )
+
+            .AllowAnyHeader()
+
+            .AllowAnyMethod()
+
+            .AllowCredentials(); 
+
     });
+
+});
+ 
+ 
+builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+ 
+// Configuraci贸n de la base de datos
 
-// Configuraci髇 de la base de datos
 builder.Services.AddDbContext<SegRutContAsisContext>(options => 
+
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+ 
+// Inyecci贸n de dependencias
 
-// Inyecci髇 de dependencias
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+
 builder.Services.AddScoped<IRolService, RolService>();
+
 builder.Services.AddScoped<IAsistenciaService, AsistenciaService>();
+
 builder.Services.AddScoped<IZonaService, ZonaService>();
+
 builder.Services.AddScoped<IClienteService, ClienteService>();
+
 builder.Services.AddScoped<IRutaService, RutaService>();
+
 builder.Services.AddScoped<IVisitaService, VisitaService>();
+
 builder.Services.AddScoped<IEvidenciaService, EvidenciaService>();
+
 builder.Services.AddScoped<ISeguimientoVendedorService, SeguimientoVendedorService>();
+
 builder.Services.AddScoped<IDireccionClienteService, DireccionClienteService>();
+
 builder.Services.AddScoped<IAsignacionClienteVendedorService, AsignacionClienteVendedorService>();
+
 builder.Services.AddScoped<IMarcarLlegadaVisitaService,  MarcarLlegadaVisitaService>();
+
 builder.Services.AddScoped<IAsignacionSupervisorVendedorService, AsignacionSupervisorVendedorService>();
+
 builder.Services.AddScoped<IReportesActividadService, ReporteActividadService>();
+ 
+ 
+// Configuraci贸n PDF
 
-
-// Configuraci髇 PDF
 QuestPDF.Settings.License = LicenseType.Community;
+ 
+// Configuraci贸n de JWT
 
-// Configuraci髇 de JWT
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+
     .AddJwtBearer(options =>
+
     {
+
         options.RequireHttpsMetadata = false; 
+
         options.SaveToken = true;
+
         options.TokenValidationParameters = new TokenValidationParameters
+
         {
+
             ValidateIssuer = true,
+
             ValidateAudience = true,
+
             ValidateLifetime = true,
+
             ValidateIssuerSigningKey = true,
+
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
+
             ValidAudience = builder.Configuration["Jwt:Audience"],
+
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+
         };
+
     });
-
+ 
 builder.Services.AddAuthorization();
-
+ 
 var app = builder.Build();
+ 
+// En produccion cors
 
-// En produccion quitar
-//app.UseCors("AllowFrontend");
-
+app.UseCors("AllowFrontend");
+ 
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
+
 {
+
     app.UseSwagger();
+
     app.UseSwaggerUI();
+
 }
-
+ 
 // En produccion habilitar
+
 app.UseHttpsRedirection();
-
+ 
 // Estado 
+
 app.MapGet("/", () => "Corriendo...");
-
+ 
 app.UseAuthentication();
+
 app.UseAuthorization();
-
+ 
 app.MapControllers();
-
+ 
 app.Run();
