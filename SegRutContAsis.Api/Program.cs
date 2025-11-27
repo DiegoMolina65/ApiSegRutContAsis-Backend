071fa993527fc62d50e16ef806710667cfd74ad6
DiegoMolina65 <diegobrian042@gmail.com>
2025-11-27 09:35:56 -0400

    UPDATE: Configuración para que acepte cualquier petición (cors)

--- SegRutContAsis.Api/Program.cs
+++ SegRutContAsis.Api/Program.cs
@@ -25,30 +25,21 @@ using SegRutContAsis.Business.Services.Rol;
 using System.Text;
  
 var builder = WebApplication.CreateBuilder(args);
- 
+
 // Add services to the container.
 // Configurar CORS con política nombrada solo desarrollo
- 
-builder.Services.AddCors(options =>
 
+builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins(
-
-                "http://localhost:5173",    
-                "https://localhost:5173",
-                "https://diego.dualbiz.net",
-                "*"
-
-            )
+        policy
+            .AllowAnyOrigin()
             .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowAnyOrigin();
+            .AllowAnyMethod();
     });
 });
- 
- 
+
 builder.Services.AddControllers();
 
 // Aumentar límite de tamaño para uploads grandes (form-data)
@@ -58,85 +49,50 @@ builder.Services.Configure<Microsoft.AspNetCore.Http.Features.FormOptions>(optio
 });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
  
 // Configuración de la base de datos
-
 builder.Services.AddDbContext<SegRutContAsisContext>(options => 
-
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
  
 // Inyección de dependencias
-
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
-
 builder.Services.AddScoped<IRolService, RolService>();
-
 builder.Services.AddScoped<IAsistenciaService, AsistenciaService>();
-
 builder.Services.AddScoped<IZonaService, ZonaService>();
-
 builder.Services.AddScoped<IClienteService, ClienteService>();
-
 builder.Services.AddScoped<IRutaService, RutaService>();
-
 builder.Services.AddScoped<IVisitaService, VisitaService>();
-
 builder.Services.AddScoped<IEvidenciaService, EvidenciaService>();
-
 builder.Services.AddScoped<ISeguimientoVendedorService, SeguimientoVendedorService>();
-
 builder.Services.AddScoped<IDireccionClienteService, DireccionClienteService>();
-
 builder.Services.AddScoped<IAsignacionClienteVendedorService, AsignacionClienteVendedorService>();
-
 builder.Services.AddScoped<IMarcarLlegadaVisitaService,  MarcarLlegadaVisitaService>();
-
 builder.Services.AddScoped<IAsignacionSupervisorVendedorService, AsignacionSupervisorVendedorService>();
-
 builder.Services.AddScoped<IReportesActividadService, ReporteActividadService>();
-
 builder.Services.AddScoped<INotificacionService, NotificacionService>();
  
 // Configuración PDF
-
 QuestPDF.Settings.License = LicenseType.Community;
  
 // Configuración de JWT
-
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
     .AddJwtBearer(options =>
-
     {
-
         options.RequireHttpsMetadata = false; 
-
         options.SaveToken = true;
-
         options.TokenValidationParameters = new TokenValidationParameters
-
         {
-
             ValidateIssuer = true,
-
             ValidateAudience = true,
-
             ValidateLifetime = true,
-
             ValidateIssuerSigningKey = true,
-
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
-
             ValidAudience = builder.Configuration["Jwt:Audience"],
-
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-
         };
-
     });
  
 builder.Services.AddAuthorization();
@@ -151,27 +107,19 @@ builder.WebHost.ConfigureKestrel(options =>
 var app = builder.Build();
  
 // En produccion cors
-
 app.UseCors("AllowFrontend");
  
 // Configure the HTTP request pipeline.
-
 if (app.Environment.IsDevelopment())
-
 {
-
     app.UseSwagger();
-
     app.UseSwaggerUI();
-
 }
  
 // En produccion habilitar
-
 app.UseHttpsRedirection();
  
 // Estado 
-
 app.MapGet("/", () => "Corriendo...");
 
 // Archivos estáticos (PDF/imagenes)
